2c387d3b6d3201915938e78e25a557a68252c26d
Fernando <me@ffaneto.com>
2019-01-30 20:32:40 -0300

    Conditional Rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,8 @@ var root = document.querySelector("#app");
 
 var app = {
   title: 'Indecision App',
-  subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ullam.'
+  subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ullam.',
+  options: ['create', 'read', 'update', 'delete']
 };
 
 var template = React.createElement(
@@ -18,7 +19,12 @@ var template = React.createElement(
   React.createElement(
     'p',
     null,
-    app.subtitle
+    app.subtitle && app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options at all"
   )
 );
 
@@ -28,35 +34,32 @@ var user = {
   location: 'Alagoas'
 };
 
+function getLocation(location) {
+  if (location) return React.createElement(
+    'p',
+    null,
+    'Location: ',
+    location
+  );
+}
+
 var templateTwo = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    user.name
+    user.name ? user.name : 'Anonymous'
   ),
   React.createElement(
     'p',
     null,
-    React.createElement(
-      'strong',
-      null,
-      'Age:'
-    ),
-    ' ',
-    user.age
+    user.age && user.age >= 18 && 'Age: ' + user.age
   ),
   React.createElement(
     'p',
     null,
-    React.createElement(
-      'strong',
-      null,
-      'Location:'
-    ),
-    ' ',
-    user.location
+    getLocation(user.location)
   )
 );
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ var root = document.querySelector("#app");
 
 var app = {
   title: 'Indecision App',
-  subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ullam.'
+  subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ullam.',
+  options: ['create', 'read', 'update', 'delete']
 }
 
 var template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    <p>{app.subtitle && app.subtitle}</p>
+    <p>{app.options.length > 0 ? "Here are your options" : "No options at all"}</p>
   </div>
 );
 
@@ -18,11 +20,16 @@ var user = {
   location: 'Alagoas'
 };
 
+function getLocation(location) {
+  if(location) 
+    return <p>Location: {location}</p>
+}
+
 var templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p><strong>Age:</strong> {user.age}</p>
-    <p><strong>Location:</strong> {user.location}</p>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    <p>{(user.age && user.age >= 18) && `Age: ${user.age}`}</p>
+    <p>{getLocation(user.location)}</p>
   </div>
 );
 
