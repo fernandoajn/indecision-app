9c2a9c4f07a8d499bb28cbde84cbfd7d3aa387f3
Fernando <me@ffaneto.com>
2019-01-30 19:47:14 -0300

    Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,71 @@
 "use strict";
 
-var template = React.createElement(
-  "p",
-  null,
-  "Hello world!!!"
-);
 var root = document.querySelector("#app");
 
-ReactDOM.render(template, root);
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Hello world!"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Lorem ipsum dolor sit amet."
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item three"
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Fernando Augusto"
+  ),
+  React.createElement(
+    "p",
+    null,
+    React.createElement(
+      "strong",
+      null,
+      "Age:"
+    ),
+    " 19"
+  ),
+  React.createElement(
+    "p",
+    null,
+    React.createElement(
+      "strong",
+      null,
+      "Location:"
+    ),
+    " Macei\xF3"
+  )
+);
+
+// Can't pass more than one var as parameter
+// We need to put them on an array
+ReactDOM.render([template, templateTwo], root);
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
-var template = <p>Hello world!!!</p>;
 var root = document.querySelector("#app");
 
-ReactDOM.render(template, root)
+var template = (
+  <div>
+    <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+      <li>Item three</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Fernando Augusto</h1>
+    <p><strong>Age:</strong> 19</p>
+    <p><strong>Location:</strong> Maceió</p>
+  </div>
+);
+
+// Can't pass more than one var as parameter
+// We need to put them on an array
+ReactDOM.render([template, templateTwo], root);
