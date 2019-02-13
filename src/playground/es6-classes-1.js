d069a38ed98b1b002d2ca84bee09248949bd01f5
Fernando <me@ffaneto.com>
2019-02-13 11:16:22 -0300

    ES6 Classes: Part II

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -5,7 +5,7 @@ class Person {
     this.age = age;
   }
 
-  getGretting() {
+  getGreeting() {
     return `Hello, my name is ${this.getName()}`;
   }
 
@@ -30,5 +30,50 @@ class Person {
   }
 }
 
-const me = new Person("Fernando", 19);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+
+  hasMajor() {
+    return !!this.major;
+  }
+
+  // Override
+  getDescription() {
+    let description = super.getDescription();
+
+    if(this.hasMajor()) {
+      description += ` And his major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, location){
+    super(name, age);
+    this.location = location;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if(this.location) {
+      greeting += ` and I'm visiting from ${this.location}.`;
+    }
+
+    return greeting;
+  }
+}
+
+const me = new Student("Fernando", 19, 'Computer Science');
 console.log(me.getDescription());
+
+const other = new Traveler("João", 16, "Maceió");
+console.log(other.getGreeting());
+
+const girl = new Traveler("Gui", 18);
+console.log(girl.getGreeting());
