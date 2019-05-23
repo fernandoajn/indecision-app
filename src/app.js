3ce3887f38c562276b0f5fd43539ed3db928a49a
Fernando Augusto <fernandoajneto@gmail.com>
2019-05-23 15:14:45 -0300

    Importing npm Modules

--- src/app.js
+++ src/app.js
@@ -1,17 +1,5 @@
-console.log("app.js is running");
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import divide, { square, add, subtract } from './utils';
-
-// console.log(square(7));
-// console.log(add(100, 50));
-// console.log(subtract(75,20));
-// console.log(divide(10,2));
-
-import { isAdult, canDrink } from './person';
-import isSenior from './person';
-
-let age = 19;
-
-console.log(isAdult(age));
-console.log(canDrink(age));
-console.log(isSenior(age));
+const template = React.createElement('h1', {}, 'Testing');
+ReactDOM.render(template, document.getElementById('app'));
