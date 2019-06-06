81255c5e58026e72a72084d6285e03b471a1e148
Fernando Augusto <fernandoajneto@gmail.com>
2019-06-06 16:02:32 -0300

    Styling the Options List

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -86,24 +86,26 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header title={title} subtitle={subtitle} />
-        <div className="container">
-        <Action 
-          handlePick={this.handlePick}
-          hasOptions={this.state.options.length > 0 ? true : false}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption 
-          handleAddOption={this.handleAddOption}
-        />
+          <div className="container">
+          <Action 
+            handlePick={this.handlePick}
+            hasOptions={this.state.options.length > 0 ? true : false}
+          />
+            <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption 
+              handleAddOption={this.handleAddOption}
+            />
+            </div>
+          </div>
         <OptionModal 
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
         />
-        </div>
       </div>
     );
   }
